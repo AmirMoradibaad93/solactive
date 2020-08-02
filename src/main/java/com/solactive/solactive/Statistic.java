f83699656b7d13af7a8ae1572dc6ba6aba052000
AmirMoradibaad <moradi_amir2009@yahoo.com>
2020-08-02 22:34:10 +0200

    Fifth commit, Statistical functions completed.

--- src/main/java/com/solactive/solactive/Statistic.java
+++ src/main/java/com/solactive/solactive/Statistic.java
@@ -3,6 +3,8 @@ package com.solactive.solactive;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Statistic {
     public static Statistic CalculateStatistic(List<Tick> ticks) {
@@ -13,7 +15,7 @@ public class Statistic {
         statistic.min = CalculateMin(ticks);
         statistic.maxDrawdown = CalculateMaxDrawdown(ticks);
         statistic.volatility = CalculateVolatility(ticks);
-        statistic.quantile = CalculateQuantite(ticks);
+        statistic.quantile = CalculateQuantile(ticks);
         statistic.twap = CalculateTwap(ticks);
         statistic.twap2 = CalculateTwap2(ticks);
         return statistic;
@@ -34,23 +36,74 @@ public class Statistic {
     }
 
     private static double CalculateTwap2(List<Tick> ticks) {
-        return 0;
+    	int sum = 0;
+    	long now = System.currentTimeMillis();
+    	for(int i = 1; i < ticks.size() ; i++) {
+    		Tick tick = ticks.get(i);
+    		if(tick.GetTimestamp()<now)
+    			sum+= Math.exp(now - tick.GetTimestamp()) * tick.GetPrice();
+    	}
+    	
+        return sum;
     }
 
     private static double CalculateTwap(List<Tick> ticks) {
-        return 0;
+    	int sum = 0;
+    	if(CalculateCount(ticks) != 0) {
+	    	Tick firstTick = ticks.get(0);
+	    	Tick secondTick = ticks.get(0);
+	    	for(int i = 1; i < ticks.size() && firstTick != null; i++) {
+	    		secondTick = ticks.get(i);
+	    		sum+= (secondTick.GetTimestamp() - firstTick.GetTimestamp()) * firstTick.GetPrice();
+	    		firstTick = secondTick;
+	    	}
+	    	if(ticks.size()>0 && secondTick != null)
+	    		sum += (ticks.get(0).GetTimestamp() + 600000 - secondTick.GetPrice())* secondTick.GetPrice();
+    	}
+        return sum;
     }
 
-    private static double CalculateQuantite(List<Tick> ticks) {
-        return 0;
+    private static double CalculateQuantile(List<Tick> ticks) {
+    	var count = CalculateCount(ticks);
+    	double price = 0;
+    	int quantile = 0;
+    	
+    	if(count != 0) {
+    		quantile = (int) Math.round(CalculateCount(ticks)*0.05) + 1;
+    		price = ticks.get(quantile).GetPrice();
+    	}    	
+        return price;
     }
 
     private static double CalculateVolatility(List<Tick> ticks) {
-        return 0;
+    	Set<String> tickInstruments = ticks.stream().map(t -> t.GetInstrument()).collect(Collectors.toSet());
+        double volatility = 0;
+        
+        for(String s:tickInstruments) {
+        	double avg= ticks.stream().filter(t -> t.GetInstrument() == s)
+        			.mapToDouble(Tick::GetPrice).sum(); 
+        	double deviationSquared = ticks.stream().filter(t -> t.GetInstrument() == s)
+        			.mapToDouble(Tick::GetPrice).map(t -> (t-avg)*(t-avg)).sum();
+        	
+        	volatility += deviationSquared/CalculateCount(ticks.stream().filter(t -> t.GetInstrument() == s).collect(Collectors.toList()));
+        }
+        
+    	return volatility;
     }
 
     private static double CalculateMaxDrawdown(List<Tick> ticks) {
-        return 0;
+    	Set<String> tickInstruments = ticks.stream().map(t -> t.GetInstrument()).collect(Collectors.toSet());
+        double maxDrawDown = 0;
+        
+        for(String s:tickInstruments) {
+        	List<Double> prices= ticks.stream().filter(t -> t.GetInstrument() == s).map(t -> t.GetPrice())
+        			.collect(Collectors.toList()); 
+        	double minPrice = Collections.min(prices);
+        	double maxPrice = Collections.max(prices);
+        	if(maxPrice-minPrice > maxDrawDown) maxDrawDown = maxPrice-minPrice;
+        }
+        
+    	return maxDrawDown;
     }
 
     private static double CalculateMin(List<Tick> ticks) {
--- src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-/*INSERT INTO index(name,abv) VALUES('Jay Alai',7.5);
-INSERT INTO index(name,abv) VALUES('Amir Moradibaad',2.3);
-INSERT INTO index(name,abv) VALUES('Luigi Pirolini',25);
-
-//COMMIT;
-*/
